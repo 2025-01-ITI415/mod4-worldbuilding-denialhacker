3ef954a35c67a25f24c6c38b44e1550972d84513
Daniel H <danielmople1@gmail.com>
2025-05-01 19:51:44 -0400

    PICKUPS MAKE NOISE

--- Assets/_Labs/02-SNAPS/Pickup.cs
+++ Assets/_Labs/02-SNAPS/Pickup.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public static int totalCollected = 0; // Shared among all items
+    private AudioSource audioSource;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.Play();
             totalCollected++;
             Debug.Log("Collected! Total: " + totalCollected);
 
-            Destroy(gameObject); // Remove the collected item
+            Destroy(gameObject, audioSource.clip.length); // Remove the collected item
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
